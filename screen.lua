9c42c90e4f5b082fe3ec8af5424a19727c49dddc
unknown <aleksandra-korenskaya@mail.ru>
2018-08-14 02:21:47 +0700

    hpbar

--- screen.lua
+++ screen.lua
@@ -4,9 +4,15 @@ function timescreen(x, y)
     love.graphics.print({3, love.timer.getTime}, 10, 10)
 end
 
+
 function screen.draw(p)
-     love.graphics.setColor(255,255,255)
-    -- love.graphics.rectangle("fill", p.hpbar, 45, 5, 15)
-     love.graphics.rectangle("fill", 100, 45, 5, 15)
+    local k = 0;
+    if p.hp ~= 0 then
+            for i = 1, p.hp/5 do
+              love.graphics.setColor(255,255,255)
+               love.graphics.rectangle("fill", p.hpbarx + k, p.hpbary, 5, 15)
+               k = k + 10
+             end
+    end
 end
 return screen
